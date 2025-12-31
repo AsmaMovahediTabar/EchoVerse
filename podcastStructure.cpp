7bbeb1c2addfe954be29cd866f094704a2cea84c
A.M <a.movahedy82@gmail.com>
2025-12-31 14:12:41 +0330

    start

---
+++ podcastStructure.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+//first phas:
+class Node{};
+
+class Tree{
+    Tree(){}
+    void insert(int channelID){}
+    void Delete(int channelID){}
+    int search(int channelID){}
+    void display(){}
+    void balance(){}
+};
+
+//second phas:
+class WeightedGraph{
+    WeightedGraph(){}
+    int dijkestra(){}
+};
+
+//phas 3:
+class TeamTree{
+    TeamTree(){}
+    int search(int id){}
+    void delet(int id){}
+    void display(){}
+};
+
+//phas 4:
+class minHeap{
+    minHeap(){}
+    int extractMin(){}
+    void Delete(){}
+    void display(){}
+    void HeapSort(){}
+};
