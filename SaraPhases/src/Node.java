d498cc62f08e47160499e2e76fc3aea0f024e53f
sara moghaddam <sarmem2006@gmail.com>
2026-01-02 22:19:14 +0330

    Add Node class (java)

---
+++ SaraPhases/src/Node.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Node {
+    private int chanelIDE;
+    private Node left;
+    private Node right;
+    private int height;
+    
+    public Node(int IDE){
+        this.chanelIDE = IDE;
+        this.left = left;
+        this.right = right;
+        this.height = 0;
+    }
+}
