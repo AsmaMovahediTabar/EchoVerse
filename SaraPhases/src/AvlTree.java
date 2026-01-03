f76536c2ac6ac37bd18060b0ed7e354125c1f5bf
sara moghaddam <sarmem2006@gmail.com>
2026-01-03 15:37:24 +0330

    Adding methods height and balanced to the AvlTree class

--- SaraPhases/src/AvlTree.java
+++ SaraPhases/src/AvlTree.java
@@ -1,4 +1,15 @@
 package src;
 
 public class AvlTree {
+    private Node root;
+    
+    public int height(Node node){
+        if(node == null){return -1;}
+        return node.height;
+    }
+    
+    public int Balanced(Node node){
+        if(node == null){return -1;}
+        return height(node.left) - height(node.right);
+    }
 }
