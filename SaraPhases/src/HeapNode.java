00be76110356fdf855aca4f60b9a104e97d72aa7
sara moghaddam <sarmem2006@gmail.com>
2026-01-06 22:28:03 +0330

    Add HeapNode class for pahse2

---
+++ SaraPhases/src/HeapNode.java
@@ -0,0 +1,11 @@
+package src;
+
+public class HeapNode{
+    int ID;
+    int Priority;
+    
+    public HeapNode(int ID , int priority){
+        this.ID = ID;
+        this.Priority = priority;
+    }
+}
