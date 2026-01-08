45e8abfbc84e19e40947aa12468323469b89d562
sara moghaddam <sarmem2006@gmail.com>
2026-01-08 19:24:29 +0330

    Menu appearance changes

--- SaraPhases/src/phase1/Main.java
+++ SaraPhases/src/phase1/Main.java
@@ -1,17 +1,47 @@
 package src.phase1;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        AvlTree avl = new AvlTree();
+        while (true) {
+            System.out.println("\n*** Manage Approved Channel ***");
+            System.out.println("*   1.Insert channel          *");
+            System.out.println("*   2.Delete channel          *");
+            System.out.println("*   3.Search channel          *");
+            System.out.println("*   4.Display                 *");
+            System.out.println("*   0.Exit                    *");
+            System.out.println("*******************************");
+            System.out.print("Enter the desired number to perform the operation: ");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter channel ID: ");
+                    int insertID = scanner.nextInt();
+                    avl.insert(insertID);
+                    break;
+                case 2:
+                    System.out.print("Enter channel ID: ");
+                    int deleteID = scanner.nextInt();
+                    avl.delete(deleteID);
+                    break;
+                case 3:
+                    System.out.print("Enter channel ID: ");
+                    int searchID = scanner.nextInt();
+                    boolean found = avl.search(searchID);
+                    System.out.println("Channel " + searchID + " is " + (found ? "" : "NOT ") + "authorized");
+                    break;
+                case 4:
+                    avl.disPlay();
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    return;
+                default:
+                    System.out.println("Invalid choice!");
+            }
         }
     }
 }
