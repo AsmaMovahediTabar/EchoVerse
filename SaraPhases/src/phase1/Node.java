a3a5b7e6a4d05ed3055b88d1df01a95115408a6e
sara moghaddam <sarmem2006@gmail.com>
2026-01-08 19:19:02 +0330

    chanelIDE --> channelID

--- SaraPhases/src/phase1/Node.java
+++ SaraPhases/src/phase1/Node.java
@@ -1,13 +1,13 @@
 package src.phase1;
 
 public class Node {
-    int chanelIDE;
+    int channelID;
     Node left;
     Node right;
     int height;
     
-    public Node(int IDE){
-        this.chanelIDE = IDE;
+    public Node(int ID){
+        this.channelID = ID;
         this.left = left;
         this.right = right;
         this.height = 0;
